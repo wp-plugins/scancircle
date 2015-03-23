c462594f3fc9f542f690a566d60b96e797b48850
aklaren <aklaren@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 23:23:46 +0000

    Release 1.27: changed 'https' option for JavaScript and link to scan page (without https)
    git-svn-id: https://plugins.svn.wordpress.org/scancircle/tags/1.27@1119261 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scancircle.php
@@ -0,0 +1,66 @@
+<?php
+	/**
+	 * Plugin Name: ScanCircle
+	 * Plugin URI: https://wordpress.org/plugins/scancircle
+	 * Description: Shortcode for the scan widget on ScanCircle partner websites. Only for registered ScanCircle partners.
+	 * Version: 1.27
+	 * Author: Arnoud Klaren
+	 */
+	add_shortcode('scancircle', 'scancircle_handler');
+
+	function scancircle_handler( $atts ) {
+		extract( shortcode_atts( array(
+			'partner' => '',
+			'language' => 'en',
+			'scanmode' => '',
+			'info' => '',
+			'prompt' => '',
+			'require' => '',
+			'parameter' => '',
+			'reference' => '',
+			'inputs' => '',
+			'category' => '',
+			'validation' => '',
+			'https' => '',
+			'jquery' => '',
+			'env' => ''	// test/development folder preceeded by a slash
+		), $atts ) );
+
+		$options = '?plugin=WordPress1.24';
+		if($scanmode)	$options .= '&amp;scanmode='.$scanmode;
+		if($info)		$options .= '&amp;info';
+		if($prompt) 	$options .= '&amp;prompt='.rawurlencode($prompt);
+		if($require) 	$options .= '&amp;require='.rawurlencode($require);		// deprecated
+		if($parameter)	$options .= '&amp;parameter='.$parameter;
+		if($reference)	$options .= '&amp;reference='.rawurlencode($reference);
+		if($inputs)		$options .= '&amp;inputs='.rawurlencode($inputs);
+		if($category)	$options .= '&amp;category='.rawurlencode($category);
+		if($validation)	$options .= '&amp;validation='.rawurlencode($validation);
+		if($jquery)		$options .= '&amp;jquery';
+		$secure = ($https != 0 ? 's' : '');
+
+		$scancircle = <<< EOL
+<div id="scancircle">
+	<a href="http://$partner.scancircle.com$env/$language/scan" id="scancircle_button" title="ScanCircle Scan">ScanCircle</a>
+</div>
+<script type="text/javascript" src="http$secure://$partner.scancircle.com$env/$language/scancircle.js$options"></script>
+EOL;
+
+		return $scancircle;
+	}
+
+	add_shortcode('phpvar', 'phpvar_handler');
+
+	function phpvar_handler( $atts ) {
+		extract( shortcode_atts( array(
+			'get' => '',
+			'post' => '',
+			'server' => ''
+		), $atts ) );
+
+		if($get)	return $_GET[$get];
+		if($post)	return $_POST[$post];
+		if($server)	return $_SERVER[$server];
+		return 'Syntax: [phpvar get|post|server="var"]';
+	}
+?>
